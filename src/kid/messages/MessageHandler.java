b332603a0915cc58def7fe5875235799bb7703cb
Brian Norman <brian.e.norman@gmail.com>
2012-01-08 14:02:17 -0600

    Preparing for management classes.
    Created an event and message handler interface for management classes. These interfaces require certain methods that help with the management of events and messages. By implementing these methods, the specifics of management classes could be ignored and all events and methods can be passed to event handlers and message handlers respectively.

---
+++ src/kid/messages/MessageHandler.java
@@ -0,0 +1,32 @@
+package kid.messages;
+
+/**
+ * The {@link MessageHandler} interface provides a simple way to require certain
+ * methods pertaining to custom Robocode messages.
+ * 
+ * @author Brian Norman
+ * @version 1.0
+ */
+public interface MessageHandler {
+
+   /**
+    * Handles the message that was specified. If the message handler does not
+    * need this message than it is ignored.
+    * 
+    * @param message
+    *           the message for the handler.
+    */
+   public void inMessage(Message message);
+
+   /**
+    * Handles the messages that are specified. If the message handler does not
+    * need one of the messages specified than it is ignored. The easiest way to
+    * implement this method is to call {@link #inMessage(Message)} for every
+    * message in the collection.
+    * 
+    * @param messages
+    *           the collection of messages for the handler.
+    */
+   public void inMessages(Iterable<Message> messages);
+
+}
