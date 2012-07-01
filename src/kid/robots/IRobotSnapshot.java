0a487e763dee8f46d53586fe265f70f4cf50fb43
Brian Norman <brian.e.norman@gmail.com>
2012-06-30 22:11:49 -0500

    Updated the documentation.
    Updated the class documentation to include the description of a blank snapshot. Updated the documentation of the getEnergy method to describe what negative energy means.

--- src/kid/robots/IRobotSnapshot.java
+++ src/kid/robots/IRobotSnapshot.java
@@ -4,6 +4,14 @@ import java.io.Serializable;
 
 /**
  * Represents a Robocode robot at a moment in time.
+ * <p>
+ * A blank robot snapshot, commonly called a blank snapshot by documentation, is
+ * a snapshot that contains no relevant information. There are only two
+ * guarantees that can be made about a blank snapshot. First, the name of the
+ * robot will be non-null but contain no characters, therefore the
+ * {@link String#isEmpty() isEmpty()} method of {@link String} will return
+ * <code>true</code>. Second, the energy of the robot will be less than zero
+ * which is considered a dead robot.
  * 
  * @author Brian Norman (KID)
  * @version 1.2
@@ -32,7 +40,8 @@ public interface IRobotSnapshot extends Serializable {
    public double getY();
 
    /**
-    * Returns the current energy of the robot.
+    * Returns the current energy of the robot. If the energy is less than zero
+    * the robot is considered dead.
     * 
     * @return the energy of the robot.
     */
