a78268f078a9f5c9ed6c1ba35ae7f2057478a471
Brian Norman <brian.e.norman@gmail.com>
2013-05-12 23:00:53 -0500

    Adding trig tables to library

---
+++ rinzlerdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir target/classes/bnorm/Rinzler.data/
+cp lib/sine.table target/classes/bnorm/Rinzler.data/sine.table
+cp lib/tangent.table target/classes/bnorm/Rinzler.data/tangnet.table
+cp lib/acosine.table target/classes/bnorm/Rinzler.data/acosine.table
